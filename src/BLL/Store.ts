a1d1b2286ee429428b4ddaf9ec91969618520836
LeonShus <leonshustov1995@gmail.com>
2021-12-10 19:00:45 +0300

    Add loadState & save to Utils, and use it in Store.

--- src/BLL/Store.ts
+++ src/BLL/Store.ts
@@ -1,6 +1,6 @@
-import React from "react"
 import {combineReducers, createStore} from "redux";
 import {counterReducer} from "./Reducers/CounterReducer";
+import {loadState, saveState} from "../Utils/LocalStorageUtils";
 
 const RootReducer = combineReducers({
     counter: counterReducer
@@ -8,6 +8,9 @@ const RootReducer = combineReducers({
 
 export type AppStateType = ReturnType<typeof RootReducer>
 
-export const store = createStore(RootReducer)
 
+export const store = createStore(RootReducer,loadState())
 
+store.subscribe(() => {
+    saveState(store.getState())
+})
